7b2cc9b421ca1f9beacbb2f1d13b7b36672a06d0
Ruben <ruben.soler@hotmail.es>
2021-06-29 11:39:40 +0200

    feat: di added

---
+++ app/src/main/java/com/solalva/expensetracker/MainApplication.kt
@@ -0,0 +1,21 @@
+package com.solalva.expensetracker
+
+import android.app.Application
+import org.koin.android.ext.koin.androidLogger
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import com.solalva.expensetracker.di.expenseTrackerModule
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        setupDependencyInjection()
+    }
+
+    private fun setupDependencyInjection() = startKoin {
+        androidLogger()
+        androidContext(this@MainApplication)
+        modules(expenseTrackerModule)
+    }
+}
---
+++ app/src/main/java/com/solalva/expensetracker/di/ExpenseTrackerModule.kt
@@ -0,0 +1,6 @@
+package com.solalva.expensetracker.di
+
+import org.koin.dsl.module
+
+val expenseTrackerModule = module {
+}
